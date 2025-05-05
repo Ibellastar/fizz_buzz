2b121a486608bfa4f6c133cff1fc3b98e7562842
Bella Bernal <bella.bernal@atlasschool.com>
2025-05-05 17:40:45 -0500

    return 0 to see if fails

--- src/fizz_buzz.c
+++ src/fizz_buzz.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 void fizz_buzz(int number) {
-    for (int i = 1; i <= number; i++) {
-        if (i % 3 ==0 && i % 5 ==0) {
-            printf("FizzBuzz\n");
-        }
-        else if (i % 3 == 0) {
-            printf("Fizz\n");
-        }
-        else if (i % 5 == 0) {
-            printf("Buzz\n");
-        } else {
-            printf("%d\n", i);
-        }
-    }
+    return 0;
+    // for (int i = 1; i <= number; i++) {
+    //     if (i % 3 ==0 && i % 5 ==0) {
+    //         printf("FizzBuzz\n");
+    //     }
+    //     else if (i % 3 == 0) {
+    //         printf("Fizz\n");
+    //     }
+    //     else if (i % 5 == 0) {
+    //         printf("Buzz\n");
+    //     } else {
+    //         printf("%d\n", i);
+    //     }
+    // }
 }
