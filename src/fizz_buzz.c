874857edd9fd187a8ac081e3578b5b41b822a602
Bella Bernal <bella.bernal@atlasschool.com>
2025-05-06 18:52:51 +0000

    test_seventeen restored

--- src/fizz_buzz.c
+++ src/fizz_buzz.c
@@ -2,10 +2,10 @@
 
 void fizz_buzz(int number) {
     for (int i = 1; i <= number; i++) {
-        if (i % 4 ==0 && i % 5 ==0) {
+        if (i % 3 ==0 && i % 5 ==0) {
             printf("FizzBuzz\n");
         }
-        else if (i % 4 == 0) {
+        else if (i % 3 == 0) {
             printf("Fizz\n");
         }
         else if (i % 5 == 0) {
